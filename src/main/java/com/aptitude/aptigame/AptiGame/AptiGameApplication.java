b17a82a281f4acede0be5454636b1c559bbf3097
Aryan <amznarya@amazon.com>
2018-06-25 00:59:19 +0530

    initial commit

---
+++ src/main/java/com/aptitude/aptigame/AptiGame/AptiGameApplication.java
@@ -0,0 +1,13 @@
+package com.aptitude.aptigame.AptiGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptiGameApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(AptiGameApplication.class, args);
+  }
+}
---
+++ src/test/java/com/aptitude/aptigame/AptiGame/AptiGameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aptitude.aptigame.AptiGame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AptiGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
