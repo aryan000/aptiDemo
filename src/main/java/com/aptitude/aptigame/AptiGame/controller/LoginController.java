29f71d7201dda21fc06be24897facc11bb261487
Aryan <amznarya@amazon.com>
2018-06-25 02:25:11 +0530

    sample form usage using post and request paramters

--- src/main/java/com/aptitude/aptigame/AptiGame/controller/LoginController.java
+++ src/main/java/com/aptitude/aptigame/AptiGame/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.aptitude.aptigame.AptiGame.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,14 +12,19 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
 
-  @ResponseBody
-  @RequestMapping("/home")
-  public String execute(){
-    return "HI i am using here";
+
+  @RequestMapping(value = "/login" , method = RequestMethod.POST)
+  public String execute(@RequestParam("username") String username , @RequestParam("password") String password){
+
+    if(username.equalsIgnoreCase("aryandtu@gmail.com") && password.equalsIgnoreCase("1234")){
+      return "Successfully authenticated";
+    }
+
+    return "username and password does not match ";
   }
 
 
-  @RequestMapping("/login")
+  @RequestMapping("/home")
   public ModelAndView start(){
     ModelAndView modelAndView = new ModelAndView();
     modelAndView.setViewName("index");
